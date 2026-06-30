72d35049fe54b38ce89efd6bf61e6a9e5b37f403
Guillaume Natier <guillaume.natier@gmail.com>
2026-06-30 08:57:13 +0200

    feat: add initial event editor

--- wp-seed-events.php
+++ wp-seed-events.php
@@ -12,3 +12,27 @@
 defined( 'ABSPATH' ) || exit;
 
 define( 'WP_SEED_EVENTS_VERSION', '0.1.0' );
+
+add_action( 'init', 'wp_seed_events_register_event_post_type' );
+
+function wp_seed_events_register_event_post_type() {
+	register_post_type(
+		'wp_seed_event',
+		array(
+			'labels'       => array(
+				'name'          => 'Évènements',
+				'singular_name' => 'Évènement',
+				'menu_name'     => 'Évènements',
+				'add_new_item'  => 'Ajouter un évènement',
+				'edit_item'     => 'Modifier l’évènement',
+				'all_items'     => 'Tous les évènements',
+			),
+			'public'       => false,
+			'show_ui'      => true,
+			'show_in_menu' => true,
+			'menu_icon'    => 'dashicons-calendar-alt',
+			'supports'     => array( 'title' ),
+			'show_in_rest' => false,
+		)
+	);
+}
